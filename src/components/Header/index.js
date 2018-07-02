21cf52b1f94ae3f5aeab747f6b06872949baa902
Anthony Castrio <anthonycastrio@gmail.com>
2018-07-02 20:09:22 +0200

    using common stylesheets

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,11 @@ import Link from 'gatsby-link'
 import './index.scss'
 
 const Header = ({ siteTitle }) => (
-  <div className="header">>
+  <div className="header">
+    <div className="temp-logo">
+      <div className="blockA" />
+      <div className="blockB" />
+    </div>
     <h1>
       <Link to="/">{ siteTitle }</Link>
     </h1>
