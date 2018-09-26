ec8dabe20a76bf73f68f147cbd513a53c4b270aa
Anthony Castrio <anthonycastrio@gmail.com>
2018-09-26 13:02:14 -0500

    add cool crawling ants effect and change cta style

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import ReactSVG from 'react-svg'
 
 import CTA from '../CTA';
 
@@ -12,6 +13,9 @@ const Splash = ({ data }) => {
     <h1 className="tagline">Stop Looking For Your Dev Team</h1>
     <h1 className="biline">You've Already Found Us</h1>
     <CTA text="Tell Me More" link="https://formico.typeform.com/to/CvuKzs" />
+    <ReactSVG
+      path="/svg/ant-path.svg"
+      className="ant-path" />
   </div>
   )
 }
