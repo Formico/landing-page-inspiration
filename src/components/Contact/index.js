1d4f1abbcd34594d8f9d2c6c346c50510d4360a0
Anthony Castrio <anthonycastrio@gmail.com>
2018-09-28 19:09:17 -0500

    added contact and footer sections to foot

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Typing from 'react-typing-animation';
 
 import CTA from '../CTA';
 
@@ -6,8 +7,28 @@ import './index.scss'
 
 const Contact = () => (
   <div className="contact">
-    <h2>Are you ready to ... build your mvp</h2>
-    <CTA text="I'm Ready" link="https://formico.typeform.com/to/CvuKzs" />
+    <h2>
+      <Typing
+        cursorClassName="cursor"
+        speed={ 100 }
+        loop={ true }>
+        Are you ready to...{" "} 
+          <Typing.Delay ms={1000} />
+          build your mvp?
+          <Typing.Delay ms={6000} />
+          <Typing.Backspace count={15} />
+          <Typing.Delay ms={2000} />
+          upgrade your architecture?
+          <Typing.Delay ms={6000} />
+          <Typing.Backspace count={27} />
+          <Typing.Delay ms={2000} />
+          launch your mobile app?
+          <Typing.Delay ms={6000} />
+          <Typing.Backspace count={46} />
+          <Typing.Delay ms={2000} />
+      </Typing>
+    </h2>
+    <CTA text="Contact Us" link="https://formico.typeform.com/to/CvuKzs" />
   </div>
 )
 
