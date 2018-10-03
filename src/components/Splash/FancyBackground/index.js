a48f1429559ae395f689e36ff3d87a78e1850e5e
Anthony Castrio <anthonycastrio@gmail.com>
2018-10-03 13:02:46 -0500

    cool new effect

--- src/components/Splash/FancyBackground/index.js
+++ src/components/Splash/FancyBackground/index.js
@@ -2,18 +2,38 @@ import React from 'react'
 
 import './index.scss'
 
-const FancyBackground = ({ data }) => {
-  return (
-  <div className="fancy-background">
-    <div className="fancy-left">
-      <div className="fancy-column">
-        <div className="fancy-cell" />
+class FancyBackground extends React.Component {
+
+  createColumns = () => {
+    let columns = [];
+    const cellTypes = ['filled', 'unfilled'];
+
+    for (let i = 0; i < 6; i++) {
+      let cells = []
+
+      for (let j = 0; j < 12; j++) {
+        let cellType = cellTypes[Math.floor(Math.random() * cellTypes.length)];
+        cells.push(<div key={ j } className={ `fancy-cell ${cellType}` } />);
+      }
+
+      columns.push(<div key={ i } className={ `fancy-column col-${i}` }>{ cells }</div>);
+    }
+
+    return columns
+  };
+
+  render() {
+    return (
+      <div className="fancy-background">
+        <div className="fancy-left">
+          { this.createColumns() }
+        </div>
+        <div className="fancy-right">
+          { this.createColumns() }
+        </div>
       </div>
-    </div>
-    <div className="fancy-right">
-    </div>
-  </div>
-  )
+    )
+  }
 }
 
 export default FancyBackground
