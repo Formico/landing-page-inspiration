a4b3dc1b3de34b919a3d9591c41ef43353057c36
Anthony Castrio <anthonycastrio@gmail.com>
2018-11-06 14:33:24 -0500

    add submit/subscribe buttons to page header

--- src/components/LandingPageInspiration/Copy/index.js
+++ src/components/LandingPageInspiration/Copy/index.js
@@ -12,7 +12,10 @@ const Copy = ({ isSticky }) => (
     <div className="content">
       <h1>Landing Page Inspiration</h1>
       <p>{`Learn from the best, so you don't look like the rest.`}</p>
-      <CTA text="Submit A Website" link="https://formico.typeform.com/to/RhOpHk" />
+      <div className="buttons">
+        <CTA text="Submit A Website" link="https://formico.typeform.com/to/RhOpHk" />
+        <CTA type="secondary" text="Subscribe" link="https://formico.typeform.com/to/TNtQ6Y" />
+      </div>
     </div>
   </div>
 )
--- src/components/LandingPageInspiration/Header/index.js
+++ src/components/LandingPageInspiration/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import CTA from '../../Index/CTA';
+
 import './index.scss'
 
 const Header = () => (
@@ -10,6 +12,10 @@ const Header = () => (
         <img alt="" className="wordmark" src="/logo/formico-wordmark.svg" />
       </div>
     </Link>
+    <div className="buttons">
+      <CTA text="Submit A Website" link="https://formico.typeform.com/to/RhOpHk" />
+      <CTA type="secondary" text="Subscribe" link="https://formico.typeform.com/to/TNtQ6Y" />
+    </div>
   </div>
 )
 
